8daf4d2a7d06c6fded10e42102db7088bdb7941c
Alex Nelson <alex_nelson@me.com>
2016-06-08 18:00:36 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by Alex Nelson on 6/8/16.
+//  Copyright © 2016 Alex Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
